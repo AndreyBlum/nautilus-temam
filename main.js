b5cc64f10f0c7e6f711b012134c1eaeb969c5783
Andrey Blum <andrey.blum1@gmail.com>
2025-03-20 20:26:19 -0300

    TEMAM

---
+++ main.js
@@ -0,0 +1,10 @@
+function temam() {
+    var audio = document.getElementById("audio")
+    if (audio.paused) {
+        audio.play()
+    } else {
+        audio.pause()
+        audio.currentTime = 0;
+    }
+    
+}
